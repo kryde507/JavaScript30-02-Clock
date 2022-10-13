75df6347747a148a98aa24d7a186e7220b8339c5
kryde507 <92607528+kryde507@users.noreply.github.com>
2022-10-13 09:12:35 -0600

    Revised code to include current time

--- app.js
+++ app.js
@@ -1,30 +1,20 @@
-const sec = document.querySelector('.second-hand');
-const min = document.querySelector('.min-hand');
-const hour = document.querySelector('.hour-hand');
+const secHand = document.querySelector('.second-hand');
+const minHand = document.querySelector('.min-hand');
+const hourHand = document.querySelector('.hour-hand');
 
-// const moveElement = () => {
-//     r += 6;
-//     sec.style.transform = `rotate(${r}deg)`;
-// }
+function setDate() {
+    const now = new Date();
+    const seconds = now.getSeconds();
+    let s = ((seconds / 60) * 360) + 90;
+    secHand.style.transform = `rotate(${s}deg)`;
 
-window.onload = function () {
-    let s = 90;
-    let m = 90;
-    let h = 90;
-    const moveSecond = () => {
-        s += 6;
-        sec.style.transform = `rotate(${s}deg)`;
-    }
-    const moveMinute = () => {
-        m += 6;
-        min.style.transform = `rotate(${m}deg)`;
-    }
-    const moveHour = () => {
-        h += 1.5;
-        hour.style.transform = `rotate(${h}deg)`;
-    }
+    const minutes = now.getMinutes();
+    let m = ((minutes / 60) * 360) + 90;
+    minHand.style.transform = `rotate(${m}deg)`;
 
-    setInterval(moveSecond, 1000);
-    setInterval(moveMinute, 60000);
-    setInterval(moveHour, 60000);
+    const hours = now.getHours();
+    let h = ((hours / 12) * 360) + 90;
+    hourHand.style.transform = `rotate(${h}deg)`;
 }
+
+window.onload = setInterval(setDate, 1000);
